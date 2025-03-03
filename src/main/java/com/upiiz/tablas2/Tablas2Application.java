b823a12720c84121ba5cbe42fd53b9184aec0060
jeanbrandon123 <jean1.brandon@gmail.com>
2025-03-02 22:36:43 -0600

    Primera versión del proyecto

---
+++ src/main/java/com/upiiz/tablas2/Tablas2Application.java
@@ -0,0 +1,13 @@
+package com.upiiz.tablas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tablas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tablas2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/upiiz/tablas2/Tablas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upiiz.tablas2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tablas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
